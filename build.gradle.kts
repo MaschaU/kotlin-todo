1873c4c5a7a05b32fba410890c81718efbe3abe8
Masa Utkovic <masa.utkovic@thermondo.de>
2025-02-13 18:22:58 +0100

    dependencies: add mocking lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
-plugins {
-    kotlin("jvm") version "2.1.0"
-}
-
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+plugins {
+    id("org.springframework.boot") version "3.2.1"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
+}
 
 repositories {
     mavenCentral()
@@ -12,11 +15,23 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
     testImplementation(kotlin("test"))
+    testImplementation("org.mockito:mockito-core:4.11.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
-    jvmToolchain(22)
+    jvmToolchain(21)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
