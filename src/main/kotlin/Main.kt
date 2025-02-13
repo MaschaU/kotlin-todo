52cd33fd010375e8b75599c22b3ef87d6d1049e1
Masa Utkovic <masa.utkovic@thermondo.de>
2025-02-13 12:58:11 +0100

    init commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+package com.personal.todo.application
+
+fun main() {
+    println("Starting Todo App...")
+    println("Mind blown...")
+
+
+    // Initialize your application components here
+    // e.g., create repositories, services, controllers, etc.
+
+    // For example:
+    // val noteRepository = InMemoryNoteRepository()
+    // val noteService = NoteService(noteRepository)
+    // ...
+}
