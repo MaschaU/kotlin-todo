826343f8a5c7a867138f7d83e1026022dee161de
Masa Utkovic <masa.utkovic@thermondo.de>
2025-02-13 14:59:53 +0100

    feat: add Note data class

--- src/main/kotlin/com/personal/todo/domain/model/Note.kt
+++ src/main/kotlin/com/personal/todo/domain/model/Note.kt
@@ -1,25 +1,8 @@
 package com.personal.todo.domain.model
 
-import com.personal.todo.domain.valueobject.NoteBody
-import com.personal.todo.domain.valueobject.NoteId
-import com.personal.todo.domain.valueobject.NoteTitle
-
 data class Note(
-    val noteTitle: NoteTitle,
-    val noteBody: NoteBody,
-    val noteId: NoteId,
+    val noteTitle: String,
+    val noteBody: String,
+    val noteId: String,
 ) {
-    companion object {
-        private const val NOTE_TITLE = "note_title"
-        private const val NOTE_BODY = "note_body"
-        private const val NOTE_ID = "note_id"
-    }
-
-    fun toMap(): Map<String, String> {
-        return mapOf(
-            NOTE_TITLE to noteTitle.toString(),
-            NOTE_BODY to noteBody.toString(),
-            NOTE_ID to noteId.toString(),
-        )
-    }
 }
