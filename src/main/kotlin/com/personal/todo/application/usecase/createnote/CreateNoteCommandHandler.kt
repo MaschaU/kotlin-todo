8cb5bd6a31ba5ee642cc8db87f2c78ea7d48de19
Masa Utkovic <masa.utkovic@thermondo.de>
2025-02-17 11:33:40 +0100

    fix: make handler class open insted of final

--- src/main/kotlin/com/personal/todo/application/usecase/createnote/CreateNoteCommandHandler.kt
+++ src/main/kotlin/com/personal/todo/application/usecase/createnote/CreateNoteCommandHandler.kt
@@ -3,9 +3,7 @@ package com.personal.todo.application.usecase.createnote
 import com.personal.todo.domain.model.Note
 import com.personal.todo.domain.repository.NoteRepository
 import java.util.*
-
-
-class CreateNoteCommandHandler(
+open class CreateNoteCommandHandler(
     private val notesRepository: NoteRepository,
 ) : CreateNoteCommandHandlerInterface {
     override fun handle(command: CreateNoteCommand): Note {
